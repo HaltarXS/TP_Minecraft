b9f4d1e120200490d891cd4f9198efe7c645799a
Helari <bastien.nanceau@gmail.com>
2015-05-15 15:00:44 +0200

    Import Crotte Class & edit Crotte color-size

--- _minecraft/src/Crotte.cpp
+++ _minecraft/src/Crotte.cpp
@@ -19,9 +19,8 @@ void Crotte::Update(float deltaTime){
 
 void Crotte::Render(){
 	glPushMatrix();
-	glTranslatef(this->Position.X + 5, this->Position.Y + 5, this->Position.Z + 2.5);
-	//glColor3f(115. / 255.f, 30 / 255.f, 15 / 255.f);
-	glColor3f(1.0, 0.0, 0.0);
-	glutSolidCube(5);
+	glTranslatef(this->Position.X + 3, this->Position.Y + 3, this->Position.Z + 1.5);
+	glColor3f(115. / 255.f, 30 / 255.f, 15 / 255.f);
+	glutSolidCube(3);
 	glPopMatrix();
 }
