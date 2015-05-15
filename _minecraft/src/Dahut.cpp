b9f3db23f51360fed926314f361c450b02e9e9d8
Polydash <polydash.dev@gmail.com>
2015-05-15 16:04:34 +0200

    Added basic methods to Dahut class

--- _minecraft/src/Dahut.cpp
+++ _minecraft/src/Dahut.cpp
@@ -1,2 +1,26 @@
+/**
+ * File : Dahut.h
+ * Author : Paul Gerst
+ * Description : Source file of the Dahut AI class
+ */
+
 #include "Dahut.h"
 
+Dahut::Dahut(NYWorld *pWorld, NYVert2Df pos):
+IABase(pWorld)
+{
+	//Init FSM
+	Initialize();
+
+	//Init type
+	type = DAHUT;
+
+	//Init transform
+	position.X = pos.X;
+	position.Y = pos.Y;
+	position.Z = pWorld->_MatriceHeights[(int) pos.X][(int) pos.Y];
+}
+
+Dahut::~Dahut()
+{
+}
--- _minecraft/src/Dahut.h
+++ _minecraft/src/Dahut.h
@@ -1,8 +1,26 @@
+/**
+ * File : Dahut.h
+ * Author : Paul Gerst
+ * Description : Header file of the Dahut AI class
+ */
+
 #ifndef DAHUT_H
 #define DAHUT_H
 
-class Dahut
+#include "IABase.h"
+
+class Dahut : public IABase
 {
+private :
+
+public :
+
+	Dahut(NYWorld *pWorld, NYVert2Df pos);
+	~Dahut();
+
+	virtual void UpdateIA(){}
+	virtual void Draw(){}
+	virtual bool States(StateMachineEvent event, MSG_Object *pMsg, int state){ return true; }
 };
 
 #endif
