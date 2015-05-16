8a43e585ffef66d1166317c774a61d48161d3dbd
Axel Deshors <axel.deshors@hotmail.fr>
2015-05-16 18:57:13 +0200

    Update lemming
    Signed-off-by: Axel Deshors <axel.deshors@hotmail.fr>

--- _minecraft/src/Lemming.cpp
+++ _minecraft/src/Lemming.cpp
@@ -1,19 +1,19 @@
 /*
 * Source file : Lemming AI class
 * Author : DESHORS Axel
-* Date : 15/05/2015
+* Date : 16/05/2015
 */
 
 #include "Lemming.h"
 
 
 Lemming::Lemming(NYWorld * _world, NYVert2Df _spawnPos)
-	:IABase(_world)
+	:IABase(_world), m_view(100.0f, 150)
 {
 	// Get the spawn position :
-	NYVert3Df _worldSpawnPos = NYVert3Df(	_spawnPos.X * NYCube::CUBE_SIZE,
-											_spawnPos.Y*NYCube::CUBE_SIZE,
-											(_world->_MatriceHeights[(int)_spawnPos.X][(int)_spawnPos.Y] + 1) * NYCube::CUBE_SIZE);
+	NYVert3Df _worldSpawnPos = NYVert3Df(_spawnPos.X * NYCube::CUBE_SIZE,
+		_spawnPos.Y * NYCube::CUBE_SIZE,
+		(_world->_MatriceHeights[(int)_spawnPos.X][(int)_spawnPos.Y] + 1) * NYCube::CUBE_SIZE);
 
 	// State machine initialization
 	IABase::Initialize();
@@ -24,15 +24,126 @@ Lemming::Lemming(NYWorld * _world, NYVert2Df _spawnPos)
 	m_currentLifePoints = m_maxLifePoints;
 	m_currentReproductionPoints = 0;
 
+	// Timer initialization
+	m_timer.start();
+
 	// Position init
 	IABase::position = _worldSpawnPos;
-
 }
 
+void Lemming::UpdateIA()
+{
+	// Update view
+	m_view.SetPosition(position);
+	m_view.SetOrientation(direction);
+
+	Update(m_timer.getElapsedMs(true));
+
+	GetVisibleCreatures();
+
+	StateMachine::Update();	// Update state machine
+}
+
+void Lemming::Update(float elapsed_time)
+{
+	m_timeElapsed += elapsed_time;
+
+	if (m_timeElapsed >= 1.f)
+	{
+		// Increase the hungerpoints
+		if (m_currentHungerPoints < m_maxHungerPoints)
+			m_currentHungerPoints++;
+		// Increase the Reproduction points
+		if (m_currentReproductionPoints < m_maxReproductionPoints)
+			m_currentReproductionPoints++;
+
+		m_timeElapsed = 0;
+	}
+
+	if (m_currentState == STATE_Move)
+	{
+
+	}
+}
+
+void Lemming::GetVisibleCreatures()
+{
+	m_visibleCreatures.clear();
+	for (int i = 0; i < CREATURE_NUM; ++i)
+	{
+		eTypeCreature type = (eTypeCreature)i;
+
+		//if (type != eTypeCreature::YETI)
+		{
+			for (int j = 0; j < (*m_entities)[type].size(); ++j)
+			{
+				if (m_view.IsInSight((*m_entities)[type][j]->position) && (*m_entities)[type][j]->GetID() != this->GetID())
+				{
+					m_visibleCreatures.push_back((*m_entities)[type][j]);
+				}
+			}
+		}
+	}
+}
+
+void Lemming::Eat()
+{
+	m_currentHungerPoints -= 100;
+	m_currentLifePoints += 10;
+	m_currentHungerPoints = max(m_currentHungerPoints, 0);
+	m_currentLifePoints = min(m_currentLifePoints, m_maxLifePoints);
+}
 
 bool Lemming::IsHungry()
 {
 	return (m_currentHungerPoints >= 100);
+}
+
+void Lemming::Draw()
+{
+	glPushMatrix();
+
+	// Draw the lemming
+	glTranslatef(position.X, position.Y, position.Z);
+
+	glColor3f(0.f, 0.2f, 0.9f);
+	glutSolidCube(7);
+
+	glPopMatrix();
+
+	m_view.DebugDraw();
+	m_path.DrawPath();
+}
+
+bool Lemming::States(StateMachineEvent event, MSG_Object * msg, int state)
+{
+	/*BeginStateMachine
+
+		OnMsg(MSG_Eat)
+	{
+		Eat();
+
+		std::cout << "Lemming : eating : Hunger = " << m_currentHungerPoints << ", Lifepoint = " << m_currentLifePoints << std::endl;
+	}
+
+
+	// States : 
+	// Initialization state :
+	State(STATE_Initialize)
+		OnEnter
+		std::cout << "Lemming init..." << std::endl;
+	PushState(STATE_Move);
+
+	// Random move state
+	State(STATE_Move)
+		OnEnter
+
+
+
+
+		EndStateMachine*/
+
+		return false;
 }
 
 Lemming::~Lemming()
--- _minecraft/src/Lemming.h
+++ _minecraft/src/Lemming.h
@@ -1,30 +1,44 @@
 /*
- * Header file : Lemming AI class
- * Author : DESHORS Axel
- * Date : 15/05/2015
- */
+* Header file : Lemming AI class
+* Author : DESHORS Axel
+* Date : 16/05/2015
+*/
 
 
 #ifndef _LEMMING_H_
 #define _LEMMING_H_
 
+
 #include "IABase.h"
+
+#include "Viewcone.h"
+#include "Path.h"
+
+#include "engine\timer.h"
+
 class Lemming : public IABase
 {
 public:
 	Lemming(NYWorld * _world, NYVert2Df _spawnPos);
 	~Lemming();
 
+	// Functions :
+	void Draw();	// Draw the lemming
+	void UpdateIA();
+	bool States(StateMachineEvent event, MSG_Object * msg, int state);
+
+
+	// Variables :
+	std::map<eTypeCreature, std::vector<IABase *>> * m_entities;	// All entities of the game
 
 private:
 	// Variables :
 	float m_walkSpeed = 0.5f;
 
-
 	// Gauge limitation
-	const int m_maxHungerPoints = 200;
-	const int m_maxLifePoints = 100;
-	const int m_maxReproductionPoints = 100;
+	static const int m_maxHungerPoints = 200;
+	static const int m_maxLifePoints = 100;
+	static const int m_maxReproductionPoints = 100;
 
 	// Real time gauge :
 	int m_currentHungerPoints;
@@ -32,10 +46,28 @@ private:
 	int m_currentLifePoints;
 
 	// Timer :
-	float m_timeElapsed = 0.f;
+	float m_timeElapsed = 0.f;	// Time elapsed reset to 0 every 1 sec (used for the HungerPoints and ReproductionPoints)
+	float m_dancingTime = 0.f;	// Time the lemming passed to dance
+	float m_randMovingTime = 0.f;	// Time the lemming passed to move around
+	NYTimer m_timer;	// Timer of the entity
+
+
+
+	// Times :
+	const float m_danceTime = 10.f;
+
+	// Perceptions :
+	std::vector<IABase*> m_visibleCreatures;	// vector of visible creatures
+	IABase * m_target;	// Target to follow for the lemming
+	Viewcone m_view;	// View of the creature
+	Path m_path;	// Path of the lemming
+	NYVert3Df m_moveVector;	// Movement vector
 
 	// Functions :
 	bool IsHungry();	// If true : the lemming is hungry
+	void GetVisibleCreatures();	// Get the visible creatures (Update the visible creatures vector)
+	void Update(float elapsed_time);	// Update timers and gauges
+	void Eat();	// Eat a piece of snow
 
 };
 
