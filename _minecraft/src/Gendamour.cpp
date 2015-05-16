623910e5547f07ec015a4c4c4a4401ef53b8a989
GrosPouletBienGras <nicolas.leroy60@outlook.fr>
2015-05-16 18:33:19 +0200

    The Gendamour can now die.

--- _minecraft/src/Gendamour.cpp
+++ _minecraft/src/Gendamour.cpp
@@ -2,7 +2,9 @@
 
 Gendamour::Gendamour(NYWorld *pWorld, NYVert2Df pos) : IABase(pWorld)
 {
-	
+	//Init FSM
+	Initialize();
+
 	type = GENDAMOUR;
 
 	positionCube.X = (int)pos.X;
@@ -12,6 +14,9 @@ Gendamour::Gendamour(NYWorld *pWorld, NYVert2Df pos) : IABase(pWorld)
 	position.X = positionCube.X*NYCube::CUBE_SIZE + NYCube::CUBE_SIZE / 2.0f;
 	position.Y = positionCube.Y*NYCube::CUBE_SIZE + NYCube::CUBE_SIZE / 2.0f;
 	position.Z = positionCube.Z*NYCube::CUBE_SIZE;
+
+	_spentTime.start();
+	_spentTimeTogether.start();
 }
 
 Gendamour::~Gendamour()
@@ -33,8 +38,29 @@ void Gendamour::Draw(){
 
 void Gendamour::UpdateIA()
 {
-	//Update FSM
+	//si le gendamour n'est pas mort, il fait des trucs
+	if (m_currentState != STATE_Dead){
+		//le gendamour fait aussi des bébé
+		if (_target != NULL && (*_target).type == GENDAMOUR && _spentTimeTogether.getElapsedSeconds() > _reproductionTime * 60){
+			cout << "this the beginning of a new life" << endl;
+			//crée un autre gendamour
+			_spentTimeTogether.start();
+		}
+		//le gendamour meurt si il a dépassé sa durée de vie 
+		if (_spentTime.getElapsedSeconds() > _lifeTime * 60 || life <= 0){
+			cout << "this is the end" << endl;
+			PushState(STATE_Dead);
+		}
+		//Le gendamour meurt si la créature qu'il suit meurt
+		if (_target != NULL && (*_target).GetState() == STATE_Dead){
+			cout << "gendamour's friend is dead, gendamour is now dead" << endl;
+			PushState(STATE_Dead);
+		}
+	}
+
+	//Update Finite State Machine
 	Update();
+
 }
 
 
@@ -43,27 +69,35 @@ bool Gendamour::States(StateMachineEvent event, MSG_Object *msg, int state)
 	BeginStateMachine
 
 		State(STATE_Initialize)
-		OnEnter
-	{
-		
+
+	OnEnter{
+		//cout << " Initialisation of the state machine d'un gendamour" << endl;
 	}
 
 		State(STATE_FindPath)
-		OnUpdate
-	{
-	
+
+	OnEnter{
+	}
+	OnUpdate{	
 	}
 
 		State(STATE_Move)
-		OnEnter
-	{
-		
-	}
 
-		OnUpdate
-	{
-		
+	OnEnter{
+	}
+	OnUpdate{
+	}
+		State(STATE_Dead)
+
+	OnEnter{
+		cout << "Gendamour is dead !" << endl;
+	}
+	OnUpdate{
 	}
 
 	EndStateMachine
+}
+
+void Gendamour::setTarget(IABase* target){
+	_target = target;
 }
--- _minecraft/src/Gendamour.h
+++ _minecraft/src/Gendamour.h
@@ -6,18 +6,27 @@
 
 #include "IABase.h"
 #include "Pathfinding.h"
+#include "engine\timer.h"
 
 class Gendamour : public IABase
 {
-public:
-	Gendamour(NYWorld *pWorld, NYVert2Df pos);
-	~Gendamour();
+	public:
+		Gendamour(NYWorld *pWorld, NYVert2Df pos);
+		~Gendamour();
 
+		//truc a override
+		virtual void Draw();
+		virtual bool States(StateMachineEvent event, MSG_Object *msg, int state);
+		virtual void UpdateIA();
 
-	//truc a override
-	virtual void Draw();
-	virtual bool States(StateMachineEvent event, MSG_Object *msg, int state);
-	virtual void UpdateIA();
+	private:
+		IABase* _target = NULL; // creature que va suivre le gendamour
+		float _lifeTime = 15;//durée de vie en min
+		float _reproductionTime = 1;//temps en minute qu'il faut passé avec un autre gendamour pour se reproduire
+		NYTimer	_spentTime ;// temps passé vivant
+		NYTimer	_spentTimeTogether;//temps passé avec un autre gendamour
+		void setTarget(IABase* target);
+
 };
 
 #endif
