2394517c5578f251c5071a84d72bcca79aa930f8
PGui <pastor.guill@gmail.com>
2015-05-17 21:35:59 +0200

    WM refactoring

--- _minecraft/src/WastedosaureManager.cpp
+++ _minecraft/src/WastedosaureManager.cpp
@@ -3,11 +3,7 @@
 
 WastedosaureManager::WastedosaureManager()
 {
-	//m_groups.resize(100000);
-	/*for (int i = 0; i < 1000; ++i)
-	{
-		m_groups[i].resize(10);
-	}*/
+
 }
 
 
@@ -15,11 +11,13 @@ WastedosaureManager::~WastedosaureManager()
 {
 }
 
+//Ajoute le wastedosaure dans la liste de wastedosaure
 void WastedosaureManager::AddWastedosaure(Wastedosaure * entity)
 {
 	m_wastosaures.push_back(entity);
 }
 
+//Renvoie vrai si tout le monde est arrivé. Obsolete
 bool WastedosaureManager::IsEveryoneArrived()
 {
 	for (int i = 0; i < m_wastosaures.size(); ++i)
@@ -34,7 +32,7 @@ bool WastedosaureManager::IsEveryoneArrived()
 	return true;
 }
 
-
+//Permet d'assigner le Wastedosaure au groupe
 void WastedosaureManager::AssignToAGroup(Wastedosaure * entity)
 {
 	m_group.clear();
@@ -53,6 +51,8 @@ void WastedosaureManager::AssignToAGroup(Wastedosaure * entity)
 
 }
 
+//Permet à un wastedosaure de trouver un partenaire libre
+//Parcours tous les partenaires, si l'un d'entre eux n'en a pas, on les assignes mutuellements
 void WastedosaureManager::FindPartner(Wastedosaure * entity)
 {
 	//cout << entity->GetID() << " Test\n";
@@ -76,6 +76,8 @@ void WastedosaureManager::FindPartner(Wastedosaure * entity)
 	}
 }
 
+//Permet de trouver un lieu de reproduction au calme
+//Trouve un lieu aux alentours de la meute pour la reproduction.
 void WastedosaureManager::FindReproductionPlace(Wastedosaure * entity1, Wastedosaure * entity2)
 {
 	if (entity1 != NULL && entity2 != NULL)
@@ -88,8 +90,12 @@ void WastedosaureManager::FindReproductionPlace(Wastedosaure * entity1, Wastedos
 	
 }
 
+//Initialise des paramètres pour l'attaque en groupe des wastedosaures
+//On parcours tous les wastedosaures et on leur assigne tous la même target si il ne sont pas mort ou si ils ne se reproduisent pas ou si ils ne sont pas en train de se suicider
+//Puis on les passe en mode attaque
 void WastedosaureManager::PrepareAttack(IABase * target)
 {
+	
 	for (int i = 0; i < m_wastosaures.size(); ++i)
 	{
 		if (m_wastosaures[i]->GetState() != STATE_Dead && m_wastosaures[i]->GetState() != STATE_Suicide && m_wastosaures[i]->GetState() != STATE_Reproduction)
