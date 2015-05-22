a5d5884a9bd92f0672b8e11a97fe743db5d681c7
Lyatus (Box) <lutopia@hotmail.fr>
2015-05-22 10:37:02 +0200

    Gevaulols actually move now (WOW VERY INCREDIBLE)

--- _minecraft/src/Gevaulol.h
+++ _minecraft/src/Gevaulol.h
@@ -12,11 +12,17 @@
 
 class Gevaulol : public IABase
 {
+private:
+	NYTimer m_timer;
+
 
 public:
 	static std::map<eTypeCreature, std::vector<IABase*>> *creatureMap;
 
 	Gevaulol(NYWorld *pWorld, NYVert2Df pos);
+	float repulsionFactor, orientationFactor, attractionFactor;
+	float repulsionRadius, orientationRadius, attractionRadius;
+	float maxSpeed, perceptionRadius;
 
 	//Overriden base class methods
 	virtual void UpdateIA();
