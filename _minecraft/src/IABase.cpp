a7c051c16b64b08833af9a473fb2c569b5572923
Lyatus (Box) <lutopia@hotmail.fr>
2015-05-24 19:09:40 +0200

    Added IABase::GetName

--- _minecraft/src/IABase.cpp
+++ _minecraft/src/IABase.cpp
@@ -43,4 +43,49 @@ void IABase::Manger()
 	t = 0;
 }
 
-
+const char* IABase::GetName() const{
+	static char WASTEDOSAURE_NAME[] = "Wastedosaure";
+	static char	DAHUT_NAME[] = "Dahut";
+	static char CAMELEON_NAME[] = "Cameleon";
+	static char	MOUCHE_NAME[] = "Mouche";
+	static char	LEMMING_NAME[] = "Lemming";
+	static char	GLACEGOUILLE_NAME[] = "Glacegouille";
+	static char	GENDAMOUR_NAME[] = "Gendamour";
+	static char	SNAKE_NAME[] = "Snake";
+	static char	NEON_NAME[] = "Neon";
+	static char	GRIFFONKITU_NAME[] = "Griffonkitu";
+	static char	FURZ_NAME[] = "Furz";
+	static char	CRABE_NAME[] = "Crabe";
+	static char	PARASITE_NAME[] = "Parasite";
+	static char	VAUTOUR_NAME[] = "Vautour";
+	static char	GEVAULOL_NAME[] = "Gevaulol";
+	static char	NEON_BLEU_NAME[] = "Neon Bleu";
+	static char	BIXI_NAME[] = "Bixi";
+	static char	NOKIA_NAME[] = "Nokia";
+	static char	YETI_NAME[] = "Yeti";
+	static char	RADIATOSAURE_NAME[] = "Radiatosaure";
+	static char UNKNOWN[] = "Unknown";
+	switch (type){
+	case WASTEDOSAURE: return WASTEDOSAURE_NAME;
+	case DAHUT: return DAHUT_NAME;
+	case CAMELEON: return CAMELEON_NAME;
+	case MOUCHE: return MOUCHE_NAME;
+	case LEMMING: return LEMMING_NAME;
+	case GLACEGOUILLE: return GLACEGOUILLE_NAME;
+	case GENDAMOUR: return GENDAMOUR_NAME;
+	case SNAKE: return SNAKE_NAME;
+	case NEON: return NEON_NAME;
+	case GRIFFONKITU: return GRIFFONKITU_NAME;
+	case FURZ: return FURZ_NAME;
+	case CRABE: return CRABE_NAME;
+	case PARASITE: return PARASITE_NAME;
+	case VAUTOUR: return VAUTOUR_NAME;
+	case GEVAULOL: return GEVAULOL_NAME;
+	case NEON_BLEU: return NEON_BLEU_NAME;
+	case BIXI: return BIXI_NAME;
+	case NOKIA: return NOKIA_NAME;
+	case YETI: return YETI_NAME;
+	case RADIATOSAURE: return RADIATOSAURE_NAME;
+	default: return UNKNOWN;
+	}
+}
